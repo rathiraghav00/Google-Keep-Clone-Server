bcb54a6efd41083f97492600fce31df79d37891b
rathiraghav00 <42902117+rathiraghav00@users.noreply.github.com>
2021-04-05 01:44:23 +0530

    Modified COR header. Delete op also works now

--- index.js
+++ index.js
@@ -20,9 +20,10 @@ app.listen(3000, function() {
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     res.header(
       "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
+      "Origin, X-Requested-With, Content-Type, Accept, *"
     );
     next();
   });

d2ca86ae395f272c60b9e6bf08c8d310edd6ae7c
rathiraghav00 <42902117+rathiraghav00@users.noreply.github.com>
2021-04-05 15:24:35 +0530

    All CRD api working for notes and auth. All debugged :)

--- index.js
+++ index.js
@@ -3,43 +3,48 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(bodyParser.json()); // for parsing application/json
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(express.static("public"));
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, *"
-    );
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, *"
+  );
+  next();
+});
 
-mongoose.connect('mongodb://localhost:27017/GKC', {useNewUrlParser : true});
+mongoose.connect("mongodb://localhost:27017/GKC", { useNewUrlParser: true });
 //////////////////////////////// CONNECTION ESTABLISHED  //////////////////////////////////////
 
 // Schema of Note
-const noteSchema = new mongoose.Schema ({
-    email_id : String,
-    title : String,
-    content : String
-})
+const noteSchema = new mongoose.Schema({
+  email_id: String,
+  title: String,
+  content: String,
+});
 
 const Note = mongoose.model("Note", noteSchema);
 
@@ -54,10 +59,10 @@ const Note = mongoose.model("Note", noteSchema);
 // console.log("note has been saved");
 
 // Schema of Auth
-const authSchema = new mongoose.Schema ({
-    email : String,
-    password : String
-})
+const authSchema = new mongoose.Schema({
+  email_id: String,
+  password: String,
+});
 
 const Auth = mongoose.model("Auth", authSchema);
 
@@ -68,168 +73,160 @@ const Auth = mongoose.model("Auth", authSchema);
 
 // auth.save();
 
-
-
 //////////////////////////////// AUTH  //////////////////////////////////////
 
-app.route('/auth')
+app
+  .route("/auth")
 
-.get(function(req, res){
-    Auth.find(function(err, foundAuths){
-        if(!err){
-            console.log("Success GET /auth request", foundAuths);
-            res.send(foundAuths);
-        }
-        else{
-            console.log("Error GET /auth request", err);
-            res.send(err);
-        }
-        
+  .get(function (req, res) {
+    Auth.find(function (err, foundAuths) {
+      if (!err) {
+        console.log("Success GET /auth request", foundAuths);
+        res.send(foundAuths);
+      } else {
+        console.log("Error GET /auth request", err);
+        res.send(err);
+      }
     });
-});
+  })
 
-.post(function(req, res){
-
+  .post(function (req, res) {
     const newUser = new Auth({
-        email : req.body.email,
-        password : req.body.password
-    })
+      email_id: req.body.email_id || req.query.email_id,
+      password: req.body.password || req.query.password,
+    });
 
-    newUser.save(function(err){
-        if(err){
-            console.log("Error while POST /auth : ", newNote, err);
-            res.send(err);
-        }
-        else{
-            console.log("Succes POST /auth : ", newUser);
-            res.send("Success");
-        }
-    })
-})
+    newUser.save(function (err) {
+      if (err) {
+        console.log("Error while POST /auth : ", newNote, err);
+        res.send(err);
+      } else {
+        console.log("Succes POST /auth : ", newUser);
+        res.send("Success");
+      }
+    });
+  })
 
-.delete(function(req, res){
-    Auth.deleteMany(function(err){
-        if(!err){
-            console.log("Success DELETE /auth");
-            res.send("Success");
-        }
-        else {
-            console.log("Error DELETE /auth ", err);
-            res.send(err);
-        }
-    })
-})
+  .delete(function (req, res) {
+    Auth.deleteMany(function (err) {
+      if (!err) {
+        console.log("Success DELETE /auth");
+        res.send("Success");
+      } else {
+        console.log("Error DELETE /auth ", err);
+        res.send(err);
+      }
+    });
+  });
 
 //////////////////////////////// AUTH - SPECIFIC ID  //////////////////////////////////////
 
-app.route('/auth/:uniqueId')
+app
+  .route("/auth/:uniqueId")
 
-.get(function(req, res){
-    Auth.findOne({_id : req.params.uniqueId}, function(err, foundAuth){
-        if(!err){
-            console.log("Success GET /auth/uniqueId : ", uniqueId, foundAuth);
-            res.send(foundAuth);
-        }
-        else{
-            console.log("Error GET /auth/uniqueId : ", uniqueId, err);
-            res.send(err);
-        }
-    })
-})
+  .get(function (req, res) {
+    Auth.findOne({ _id: req.params.uniqueId }, function (err, foundAuth) {
+      if (!err) {
+        console.log(
+          "Success GET /auth/uniqueId : ",
+          req.params.uniqueId,
+          foundAuth
+        );
+        res.send(foundAuth);
+      } else {
+        console.log("Error GET /auth/uniqueId : ", req.params.uniqueId, err);
+        res.send(err);
+      }
+    });
+  })
 
-.delete(function(req, res){
-    Auth.deleteOne({_id : req.params.uniqueId},
-        function(err)
-        {
-            if(!err){
-                console.log("Success DELETE /auth/uniqueId", uniqueId);
-                res.send("Success");
-            }
-            else
-            {
-                console.log("Error DELETE /auth/uniqueId", uniqueId);
-                res.send(err);
-            }
-        }
-    )
-})
+  .delete(function (req, res) {
+    Auth.deleteOne({ _id: req.params.uniqueId }, function (err) {
+      if (!err) {
+        console.log("Success DELETE /auth/uniqueId", req.params.uniqueId);
+        res.send("Success");
+      } else {
+        console.log("Error DELETE /auth/uniqueId", req.params.uniqueId);
+        res.send(err);
+      }
+    });
+  });
 
 //////////////////////////////// NOTES //////////////////////////////////////
 
-app.get('/notes', function(req, res){
-    Note.find(function(err, foundNotes){
-        if(!err){
-            console.log("Success GET /notes", foundNotes);
-            res.send(foundNotes);
-        }
-        else{
-            console.log("Error while GET /notes", err);
-            res.send(err);
-        }    
-    })
-})
+app.get("/notes", function (req, res) {
+  Note.find(function (err, foundNotes) {
+    if (!err) {
+      console.log("Success GET /notes", foundNotes);
+      res.send(foundNotes);
+    } else {
+      console.log("Error while GET /notes", err);
+      res.send(err);
+    }
+  });
+});
 
-app.post('/notes', function(req, res){  
-    const newNote = new Note({
-        email : req.body.email,
-        title : req.body.title,
-        content : req.body.content
-    })
+app.post("/notes", function (req, res) {
+  const newNote = new Note({
+    email_id: req.body.email_id || req.query.email_id,
+    title: req.body.title || req.query.title,
+    content: req.body.content || req.query.content,
+  });
 
-    newNote.save(function(err){
-        if(err){
-            console.log("Error POST /notes", newNote, err);
-            res.send(err);
-        }
-        else{
-            console.log("Success POST /notes", newNote);
-            res.send("Success");
-        }
-    })
-})
+  console.log(newNote);
 
-app.delete('/notes', function(req, res){
+  newNote.save(function (err) {
+    if (err) {
+      console.log("Error POST /notes", newNote, err);
+      res.send(err);
+    } else {
+      console.log("Success POST /notes", newNote);
+      res.send("Success");
+    }
+  });
+});
 
-    Note.deleteMany(function(err){
-        if(!err){
-            console.log("All notes deleted");
-            res.send("Success");
-        }
-        else {
-            console.log("Error while deleting all nodes", err);
-            res.send(err);
-        }
-    })
-})
+app.delete("/notes", function (req, res) {
+  Note.deleteMany(function (err) {
+    if (!err) {
+      console.log("All notes deleted");
+      res.send("Success");
+    } else {
+      console.log("Error while deleting all nodes", err);
+      res.send(err);
+    }
+  });
+});
 
 //////////////////////////////// NOTES - SPECIFIC ID  //////////////////////////////////////
 
-app.route('/notes/:uniqueId')
+app
+  .route("/notes/:uniqueId")
 
-.get(function(req, res){
+  .get(function (req, res) {
+    Note.findOne({ _id: req.params.uniqueId }, function (err, foundNote) {
+      if (!err) {
+        console.log(
+          "Success GET /notes/uniqueId ",
+          req.params.uniqueId,
+          foundNote
+        );
+        res.send(foundNote);
+      } else {
+        console.log("Error GET /notes/uniqueId ", req.params.uniqueId, err);
+        res.send(err);
+      }
+    });
+  })
 
-    Note.findOne({_id : req.params.uniqueId}, function(err, foundNote){
-        if(!err){
-            console.log("Success GET /notes/uniqueId ", uniqueId, foundNote);
-            res.send(foundNote);
-        }
-        else{
-            console.log("Error GET /notes/uniqueId ", uniqueId, err);
-            res.send(err);
-        }
-    })
-})
-
-.delete(function(req, res){
-    Note.deleteOne({_id : req.params.uniqueId},function(err){
-            if(!err){
-                console.log("Success DELETE /notes/uniqueId", uniqueId);
-                res.send("Success");
-            }
-            else
-            {
-                console.log("Error DELETE /notes/uniqueId", uniqueId);
-                res.send(err);
-            }
-        });
-});
+  .delete(function (req, res) {
+    Note.deleteOne({ _id: req.params.uniqueId }, function (err) {
+      if (!err) {
+        console.log("Success DELETE /notes/uniqueId", req.params.uniqueId);
+        res.send("Success");
+      } else {
+        console.log("Error DELETE /notes/uniqueId", req.params.uniqueId);
+        res.send(err);
+      }
+    });
+  });

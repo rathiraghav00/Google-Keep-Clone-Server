a20031059aad7b4a61f6aa6991382d925f28e7ae
rathiraghav00 <42902117+rathiraghav00@users.noreply.github.com>
2021-04-06 17:31:20 +0530

    Added procfile, gitignore and changed ports

--- index.js
+++ index.js
@@ -17,10 +17,16 @@ app.use(
   })
 );
 
+const port = process.env.PORT;
+
+if (port == null || port == "") {
+  port = 3000;
+}
+
 app.use(express.static("public"));
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(port, function () {
+  console.log("Server started successfully");
 });
 
 app.use((req, res, next) => {
